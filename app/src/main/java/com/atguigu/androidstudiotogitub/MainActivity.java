35897da6b5518ce7057176e8b91821884d69c226
Administrator <332168460@qq.com>
2017-05-18 10:10:31 +0800

    删除后第一次上传

--- app/src/main/java/com/atguigu/androidstudiotogitub/MainActivity.java
+++ app/src/main/java/com/atguigu/androidstudiotogitub/MainActivity.java
@@ -10,5 +10,8 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         System.out.println("hhe");
+        System.out.println("hhe");
+        System.out.println("hhe");
+        System.out.println("hhe");
     }
 }
